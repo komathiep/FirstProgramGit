a6d1df168f37869bfaea027fccf4e533e49084eb
komathiep <komathi.guna@gmail.com>
2023-04-27 18:53:20 +0530

    My Task

---
+++ src/org/test/SampleProgramGit.java
@@ -0,0 +1,31 @@
+package org.test;
+
+
+public class SampleProgramGit {
+	public void Data() {
+			short empId=333;
+			String empName="Komathi";
+			String empEmail="komathi.guna@gmamail.com";
+			long empPhoneno=8686178485l;
+			float empSalary=25000;
+			char gender='f';
+			String empCity="narayanavanam";
+			System.out.println("My id:"+empId);
+			System.out.println("My name is:"+empName);
+			System.out.println("My Email:"+empEmail);
+			System.out.println("My number:"+empPhoneno);
+			System.out.println("My salary:"+empSalary);
+			System.out.println("My gender:"+gender);
+			System.out.println("My city:"+empCity);
+			
+			
+			
+			}
+		public static void main(String[] args) {
+			SampleProgramGit e=new SampleProgramGit();
+			e.Data();
+		
+		
+	}
+
+}
